7fca2a81d5ac47bf0bfca084424f8d27c19d957c
ilya_komarov <binary.reptile@gmail.com>
2023-02-06 17:59:25 +0300

    feat: update hop logic

--- nvim/lua/plugins/nvim_hop.lua
+++ nvim/lua/plugins/nvim_hop.lua
@@ -1,5 +1,10 @@
+    return {
+      "phaazon/hop.nvim",
+      branch = 'v2',
+      event = "VimEnter",
+    },
 local keymap = vim.keymap
-local hop = require"hop"
+local hop = require "hop"
 hop.setup {
   case_insensitive = true,
   char2_fallback_key = "<CR>",
@@ -8,13 +13,13 @@ hop.setup {
 
 local directions = require"hop.hint".HintDirection
 
-keymap.set("n", "<leader>b", "", {
+keymap.set("n", "b", "", {
   callback = function()
     hop.hint_words({ direction = directions.BEFORE_CURSOR })
   end,
 })
 
-keymap.set("n", "<leader>w", "", {
+keymap.set("n", "w", "", {
   callback = function()
     hop.hint_words({ direction = directions.AFTER_CURSOR })
   end,
