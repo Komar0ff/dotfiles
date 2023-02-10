a0f5088959e52aba6bd561271c2feb99cc1e2113
ilya_komarov <binary.reptile@gmail.com>
2023-02-11 00:43:56 +0300

    chore: remove trash

--- nvim/README.md
+++
@@ -1,2 +0,0 @@
-To automatically open files in vim by default on the mac, use the instructions below
-https://gist.github.com/rpapallas/31dfdf0626d078a357fccd46cdf6eafd
--- nvim/lua/plugins/nvim-cmp.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.start({
-    name = 'my-server-name',
-      cmd = {'vim-language-server'},
-        root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
-      })
